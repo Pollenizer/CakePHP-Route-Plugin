04f4351d469f996778f15bff0ba9e1c6c68bb845
Robert Love <robert@pollenizer.com>
2012-03-18 22:13:50 +1100

    Doco

--- Config/Schema/db_route.sql
+++ Config/Schema/db_route.sql
@@ -1,18 +1,20 @@
-# $Id$
 #
-# Copyright 2012, Pollenizer Pty. Ltd.
+# Routes
 #
 # Licensed under The MIT License
-# Redistributions of files must retain the above copyright notice.
-# MIT License (http://www.opensource.org/licenses/mit-license.php)
-
+# Redistributions of files must retain the below copyright notice.
+#
+# @author     Robert Love <robert@pollenizer.com>
+# @copyright  Copyright 2012, Pollenizer Pty. Ltd. (http://pollenizer.com)
+# @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
+#
 CREATE TABLE `routes` (
  `id` INT(10) NOT NULL AUTO_INCREMENT,
- `uid` CHAR(40) NOT NULL,
  `name` VARCHAR(255) NOT NULL,
  `value` VARCHAR(255) NOT NULL,
+ `key` CHAR(40) NOT NULL,
  PRIMARY KEY (`id`),
- UNIQUE KEY `uid` (`uid`),
  KEY `name` (`name`),
- KEY `value` (`value`)
+ KEY `value` (`value`),
+ UNIQUE KEY `key` (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
